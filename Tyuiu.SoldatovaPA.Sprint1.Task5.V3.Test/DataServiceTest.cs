058111583acba2cd158e6605eac037eb04ff189b
PolinaSold <clevnay@mail.ru>
2025-11-23 01:33:41 +0500

    Task5

---
+++ Tyuiu.SoldatovaPA.Sprint1.Task5.V3.Test/DataServiceTest.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Tyuiu.SoldatovaPA.Sprint1.Task5.V3.Lib;
+
+namespace Tyuiu.SoldatovaPA.Sprint1.Task5.V3.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCalculate_ExampleFromCondition()
+        {
+            DataService ds = new DataService();
+            int k = 130985;
+            int wait = 9;
+            int res = ds.Calculate(k);
+            Assert.AreEqual(wait, res);
+        }
+
+        [TestMethod]
+        public void ValidCalculate_SixDigitNumber()
+        {
+            DataService ds = new DataService();
+            int k = 123456;
+            int wait = 4;
+            int res = ds.Calculate(k);
+            Assert.AreEqual(wait, res);
+        }
+
+        [TestMethod]
+        public void ValidCalculate_ThreeDigitNumber()
+        {
+            DataService ds = new DataService();
+            int k = 789;
+            int wait = 7;
+            int res = ds.Calculate(k);
+            Assert.AreEqual(wait, res);
+        }
+
+        [TestMethod]
+        public void ValidCalculate_FourDigitNumber()
+        {
+            DataService ds = new DataService();
+            int k = 4567;
+            int wait = 5;
+            int res = ds.Calculate(k);
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.SoldatovaPA.Sprint1.Task5.V3/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using Tyuiu.SoldatovaPA.Sprint1.Task5.V3.Lib;
+
+namespace Tyuiu.SoldatovaPA.Sprint1.Task5.V3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.WriteLine("***************************************************");
+            Console.WriteLine("* ПР №1 | Вариант 3 | ИСПБ-25-1                   *");
+            Console.WriteLine("* Получение третьей цифры от конца числа          *");
+            Console.WriteLine("* Автор: Солдатова П.А.                           *");
+            Console.WriteLine("***************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                        *");
+            Console.WriteLine("* Присвоить целой переменной h третью от конца    *");
+            Console.WriteLine("* цифру в записи положительного целого числа k    *");
+            Console.WriteLine("* (например, если k=130985, то h=9).              *");
+            Console.WriteLine("***************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                *");
+            Console.WriteLine("***************************************************");
+
+            try
+            {
+                Console.Write("Введите положительное целое число k: ");
+                int k = Convert.ToInt32(Console.ReadLine());
+
+                if (k > 0)
+                {
+                    if (k >= 100)
+                    {
+                        Console.WriteLine("***************************************************");
+                        Console.WriteLine("* РЕЗУЛЬТАТ:                                     *");
+                        Console.WriteLine("***************************************************");
+
+                        int h = ds.Calculate(k);
+                        Console.WriteLine($"Третья цифра от конца числа {k} = {h}");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Ошибка: число должно содержать минимум 3 цифры.");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Ошибка: число должно быть положительным.");
+                }
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Ошибка: введите целое число.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Произошла ошибка: {ex.Message}");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
