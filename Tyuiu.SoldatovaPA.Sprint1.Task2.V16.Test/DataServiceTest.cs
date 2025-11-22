5a0039b2df3f6b322fa933de927b94329f29ebe8
PolinaSold <clevnay@mail.ru>
2025-11-23 00:31:25 +0500

    Task2

---
+++ Tyuiu.SoldatovaPA.Sprint1.Task2.V16.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Tyuiu.SoldatovaPA.Sprint1.Task2.V16.Lib;
+
+namespace Tyuiu.SoldatovaPA.Sprint1.Task2.V16.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCalculatePerimetrCircle()
+        {
+            DataService ds = new DataService();
+            int radius = 5;
+            double wait = 31.416;
+            double res = ds.CalculatePerimetrCircle(radius);
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.SoldatovaPA.Sprint1.Task2.V16/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Tyuiu.SoldatovaPA.Sprint1.Task2.V16.Lib;
+
+namespace Tyuiu.SoldatovaPA.Sprint1.Task2.V16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.WriteLine("***************************************************");
+            Console.WriteLine("* ПР №1 | Вариант 16 | ИСП-б-25-1                 *");
+            Console.WriteLine("* Вычисление периметра круга                      *");
+            Console.WriteLine("* Автор: Солдатова П.А.                           *");
+            Console.WriteLine("***************************************************");
+
+            Console.Write("Введите радиус круга: ");
+            int radius = Convert.ToInt32(Console.ReadLine());
+
+            double result = ds.CalculatePerimetrCircle(radius);
+            Console.WriteLine($"Периметр круга: {result:F3}");
+
+            Console.ReadKey();
+        }
+    }
+}
